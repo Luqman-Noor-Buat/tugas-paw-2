7d7bfe049c597b82b2223f5989ccb7fb7070a1c8
Luqman-Noor-Buat <luqman.utm1@gmail.com>
2022-03-09 01:01:07 +0700

    Membuat file variable.php

---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+    $a = 4;
+    echo "Nilai a adalah : ".$a."<br>";
+    $b = 2;
+    echo "Nilai b adalah : ".$b."<br>";
+    $c = 3;
+    echo "Nilai c adalah : 3".$c."<br>";
+    $hasil = $a + $b / $c;
+    echo "Maka hasil dari ".$a." + ".$b." / ".$c." = ".$hasil;
+?>
