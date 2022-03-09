ed9b9cfe9f54629032f79a82f6201e05c53936c8
Luqman-Noor-Buat <luqman.utm1@gmail.com>
2022-03-09 22:06:59 +0700

    Menambah keterangan pada command dan menambah tag

--- hello.php
+++ hello.php
@@ -1,6 +1,5 @@
 <?php
-    echo"Nama : Luqman Noor Buat";
-    echo"<br>";
-    echo"NIM  : 200411100206";
-    //ini adalah isi dari file hello
+    echo"<h1>Nama : Luqman Noor Buat</h1>";// tag h1 berfungsi untuk membuat teks menjadi besar dan tebal
+    echo"<br>"; //tag <br> berfungsi untuk enter
+    echo"<h1>NIM  : 200411100206</h1>"
 ?>
