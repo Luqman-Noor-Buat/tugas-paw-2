f98eba0f48232ccede9223385b598842b8cea695
Luqman-Noor-Buat <luqman.utm1@gmail.com>
2022-03-09 22:41:12 +0700

    Menambahkan beberapa tag html

--- fungsi.php
+++ fungsi.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nomor 3</title>
+</head>
+<body>
+  <h3>
+  <?php
     function kota() {
         echo "Demak ";
       }
@@ -6,3 +16,6 @@
       echo kota()."beradai di antara Kota Semarang, Kabupaten Kudus, Kabuaten Jepara, dan Kabupaten Grobogan. <br>";
       echo kota()."memiliki 14 kecamatan dengan 249 desa/kelurahan. <br>";
 ?>
+  </h3>
+</body>
+</html>
