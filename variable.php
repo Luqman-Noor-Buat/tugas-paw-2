bdfc280150ae1628999818f3b0fce73f0dabc633
Luqman-Noor-Buat <luqman.utm1@gmail.com>
2022-03-09 22:15:24 +0700

    Menambahkan garis diantara kalimat

--- variable.php
+++ variable.php
@@ -1,10 +1,15 @@
 <?php
+    $garis = "------------------------------------------------------------<br>";
     $a = 4;
-    echo "Nilai a adalah : ".$a."<br>";
+    echo "Nilai a adalah : $a <br>";
+    echo "$garis";
     $b = 2;
-    echo "Nilai b adalah : ".$b."<br>";
+    echo "Nilai b adalah : $b<br>";
+    echo $garis;
     $c = 3;
-    echo "Nilai c adalah : 3".$c."<br>";
+    echo "Nilai c adalah : $c<br>";
+    echo $garis;
     $hasil = ($a + $b) / $c;
-    echo "Maka hasil dari (".$a." + ".$b.") / ".$c." = ".$hasil;
+    echo "Maka hasil dari ($a + $b) / $c = $hasil <br>";
+    echo $garis;
 ?>
